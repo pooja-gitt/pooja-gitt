29462cc27fec324c28626fc06a7800509d2924cb
pooja-gitt <104057783+pooja-gitt@users.noreply.github.com>
2022-04-20 00:26:57 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+paypal.Buttons({
+        // Sets up the transaction when a payment button is clicked
+        createOrder: (data, actions) => {
+          return actions.order.create({
+            purchase_units: [{
+              amount: {
+                value: '70.00' // Can also reference a variable or function
+              }
+            }]
+          });
+        },
+        // Finalize the transaction after payer approval
+        onApprove: (data, actions) => {
+          return actions.order.capture().then(function(orderData) {
+            // Successful capture! For dev/demo purposes:
+            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+            const transaction = orderData.purchase_units[0].payments.captures[0];
+            alert(`Transaction ${transaction.status}: ${transaction.id}\n\nSee console for all available details`);
+          
+          });
+        }
+      }).render('#paypal-button-container');
